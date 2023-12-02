04ba14bc7c75147ab156ceaf169f3f85238d3700
“goshaaaa59” <eldargordienko@gmail.com>
2023-12-02 19:06:35 +0200

    git config --global core.autocrlf true

--- library.py
+++ library.py
@@ -1,4 +1,4 @@
 def сm_to_inches_converter(length: float) -> float:
-    inch = round(eval(str(length) + '/ 2.54'), 6)
+    inch = round(eval(str(length) + "/ 2.54"), 6)
 
     return inch
--- test_library.py
+++ test_library.py
@@ -1,16 +1,20 @@
 import library
 
+
 def test_1_сm_to_inches_converter():
     result = library.сm_to_inches_converter(2.54)
     assert result == 1.0
+
 
 def test_2_сm_to_inches_converter():
     result = library.сm_to_inches_converter(-10)
     assert result == -3.937008
 
+
 def test_3_сm_to_inches_converter():
     result = library.сm_to_inches_converter(0)
     assert result == 0.0
+
 
 def test_4_сm_to_inches_converter():
     result = library.сm_to_inches_converter(0.03)
